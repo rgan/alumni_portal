c6fc0572c94da93fb96cd7537ba6f695295673fc
rgan <rajaram.ganeshan@gmail.com>
2011-10-09 17:55:49 -0400

    Fixed bug in seed data

--- app/controllers/alumni_controller.rb
+++ app/controllers/alumni_controller.rb
@@ -1,15 +1,16 @@
 class AlumniController < ApplicationController
+   OPTIONS = {:except => [:encrypted_password, :salt, :email]}
 
   # POST /alumni
   def create
     @alumnus = Alumnus.create(params[:alumnus])
     Sunspot.commit
-    respond_with(@alumnus)
+    respond_with(@alumnus, OPTIONS)
   end
 
   def search
     @result = Alumnus.do_search(params[:search_text])
-    respond_with(@result)
+    respond_with(@result, OPTIONS)
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
 Alumnus.create(:email => 'admin@admin.com', :admin => true, :first_name => 'admin', :last_name => 'admin',
                :ug_college => 'cmu', :password => 'foobar',
-               :pg_college => 'cmu', :specialty => 'Internal Medicine', :sub_specialty => 'Endocrinology')
+               :pg_college => 'cmu', :specialty => 'Internal Medicine', :sub_specialty => 'Endocrinology',
+               :address => {:city => "pittsburgh", :country => "Aruba"})
