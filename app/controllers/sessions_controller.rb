15227e438f2d88b1308ff179348a3ef37e6fcede
rgan <rajaram.ganeshan@gmail.com>
2011-10-08 20:52:19 -0400

    Setup login/register/search worflow in the ui

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,10 @@
 class SessionsController < ApplicationController
   COOKIE_KEY = 'alumni_id'
 
+  def index
+    @user = session[:user_id] ? Alumnus.find(session[:user_id]) : nil
+  end
+
   def create
     user = Alumnus.authenticate(params[:email], params[:password])
     if user
